84af730b55996065fae885361dced8beb7037d9b
GiraeffleAeffle <maximilian.stahl@gmx.de>
2026-06-30 20:17:16 +0200

    Configure verifier-backed staging checkout

--- deploy/hetzner-staging/scripts/generate-staging-secrets.sh
+++ deploy/hetzner-staging/scripts/generate-staging-secrets.sh
@@ -26,6 +26,11 @@ db_root_password="$(random_hex 24)"
 shopbridge_token="$(random_hex 32)"
 merchant_id="agentcart-staging-shop"
 merchant_name="AgentCart Staging Shop"
+tempo_recipient_address="0x0000000000000000000000000000000000a6e7c"
+payment_verifier_token="$(random_hex 32)"
+stripe_sandbox_secret_key="sk_test_agentcart_staging_demo_not_real"
+stripe_profile_id="agentcart_staging_demo_profile"
+mpp_secret_key="$(openssl rand -base64 32 | tr -d '\n')"
 
 cat > "$SECRETS_ENV_FILE" <<EOF
 STAGING_DOMAIN=woo-staging.agentcart.eu
@@ -37,6 +42,11 @@ STAGING_DB_ROOT_PASSWORD=$db_root_password
 STAGING_SHOPBRIDGE_TOKEN=$shopbridge_token
 STAGING_MERCHANT_ID=agentcart-staging-shop
 STAGING_MERCHANT_NAME=AgentCart Staging Shop
+STAGING_TEMPO_RECIPIENT_ADDRESS=$tempo_recipient_address
+STAGING_PAYMENT_VERIFIER_TOKEN=$payment_verifier_token
+STAGING_STRIPE_SANDBOX_SECRET_KEY=$stripe_sandbox_secret_key
+STAGING_STRIPE_PROFILE_ID=$stripe_profile_id
+STAGING_MPP_SECRET_KEY=$mpp_secret_key
 EOF
 
 cat > "$SECRETS_YAML_FILE" <<EOF
@@ -50,6 +60,11 @@ staging:
   shopbridge_token: "$shopbridge_token"
   merchant_id: "$merchant_id"
   merchant_name: "$merchant_name"
+  tempo_recipient_address: "$tempo_recipient_address"
+  payment_verifier_token: "$payment_verifier_token"
+  stripe_sandbox_secret_key: "$stripe_sandbox_secret_key"
+  stripe_profile_id: "$stripe_profile_id"
+  mpp_secret_key: "$mpp_secret_key"
 EOF
 
 chmod 600 "$SECRETS_ENV_FILE" "$SECRETS_YAML_FILE"
